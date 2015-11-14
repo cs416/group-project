17887bdc5b990be0ab24c1f4074c70e694a84a1e
cameronDz <cameron.dziurgot@my.ccsu.edu>
2015-11-14 01:23:00 -0500

    Kevin's Update
    Pushed Kevin's update from email.

---
+++ MVC_Application/MVC_Application/src/java/edu/ccsu/validators/StateValidator.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ccsu.validators;
+
+
+import edu.ccsu.beans.Customer;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class StateValidator implements ConstraintValidator<State, Customer> {
+
+    @Override
+    public void initialize(State constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(Customer value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return false;
+        }
+
+        if (value.getUsstate() == null) {
+            return false;
+        }
+
+        return value.getUsstate().length() == 2;
+    }
+}
