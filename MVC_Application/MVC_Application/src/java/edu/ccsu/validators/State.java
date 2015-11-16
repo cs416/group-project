9b4e60a120b59c6e0feacafbfcbc3307d98585f2
Kevin Middleton <Kevin@10.8.72.37>
2015-11-16 12:34:28 -0500

    11/16 - 12:34 Commit

--- MVC_Application/MVC_Application/src/java/edu/ccsu/validators/State.java
+++ MVC_Application/MVC_Application/src/java/edu/ccsu/validators/State.java
@@ -10,9 +10,10 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import javax.validation.Constraint;
 import javax.validation.Payload;
 
-
+@Constraint(validatedBy = {StateValidator.class})
 @Target({ElementType.FIELD, ElementType.PARAMETER})
 @Retention(value = RetentionPolicy.RUNTIME)
 @Documented
